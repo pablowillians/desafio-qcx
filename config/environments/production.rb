22e4520cbdc27a8696f141a960bb2271eaa61f90
Pablo Willian Suchewshy <pblwlln@gmail.com>
2020-06-11 23:51:03 -0300

    Add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.hosts << ".ngrok.io"
+
+  config.action_mailer.default_url_options = { host: '.ngrok.io', port: 3000 }
 end
