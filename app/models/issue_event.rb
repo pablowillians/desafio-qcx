5ffdb93ecf9bd30aa2f9999b0cf4d2666098b8a0
Pablo Willian Suchewschy <pblwlln@gmail.com>
2020-06-14 22:49:03 -0300

    Change serializer for IssueEvent to serialize field event_payload as JSON

--- app/models/issue_event.rb
+++ app/models/issue_event.rb
@@ -1,5 +1,5 @@
 class IssueEvent < ApplicationRecord
   validates :issue_number, presence: true, numericality: { only_integer: true }
 
-  serialize :event_payload
+  serialize :event_payload, JSON
 end
